05c3b246ff944b4b76537067eb7d7e866b552e2c
sundeepreddy123 <r.sundeepreddy786@gmail.com>
2025-11-01 19:06:59 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,3 +9,38 @@ provider "aws" {
 terraform {
   backend "s3" {}
 }
+
+provider "helm" {
+  kubernetes { 
+    host          =  module.eks.cluster_endpoint
+    cluster_ca_certificate  =  base64code(module.eks.cluster_certificate_authorith_data)
+    exec {
+      api_version   =  "client.authentication.k8s.io/v1beta1"
+      command       = "aws"
+
+      args = var.env == "dev" ? args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name] : ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--role-arn", var.user_role_arn] 
+    }
+  }
+}
+
+provider "kubectl" {
+  apply_retry_count       = 5
+  host                    = module.eks.cluster_endpoint
+  cluster_ca_certificate  = base64decode(module.eks.cluster_certificate_authority_date)
+
+  load_config_file        = false
+
+  exec {
+    api_version    =  "client.authentication.k8s.io/v1beta1"
+    command        =  "aws"
+
+    args = var.env  == "dev" ? args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name] : ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--role-arn", var.user_role_arn] 
+}
+
+}
+
+provider "datadog" {
+  api_key = var.datadog_api_key
+  app_key = var.datadog_app_key
+  api_url = var.datadog_api_url
+}
