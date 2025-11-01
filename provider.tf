da03fd465dd21caa2f0ea079cdc304c65c9f8272
sundeepreddy123 <r.sundeepreddy786@gmail.com>
2025-11-01 18:42:32 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,11 +7,5 @@ provider "aws" {
   }
 
 terraform {
-  backend "s3" {
-    bucket         = "my-terraform-state-bucket"
-    key            = "env/dev/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "my-terraform-lock-table"
-    encrypt        = true
-  }
+  backend "s3" {}
 }
