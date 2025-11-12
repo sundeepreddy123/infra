2bcfdc3431b47b3481e8f4de5074076da29303e1
sundeepreddy123 <r.sundeepreddy786@gmail.com>
2025-11-12 14:07:41 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -26,7 +26,7 @@ provider "helm" {
 provider "kubectl" {
   apply_retry_count       = 5
   host                    = module.eks.cluster_endpoint
-  cluster_ca_certificate  = base64decode(module.eks.cluster_certificate_authority_date)
+  cluster_ca_certificate  = base64decode(module.eks.cluster_certificate_authority_data)
 
   load_config_file        = false
 
