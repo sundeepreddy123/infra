1f4ca8f86b44fcbb57e6c0e505f5db973da3ce66
sundeepreddy123 <r.sundeepreddy786@gmail.com>
2025-11-12 13:50:57 +0100

    Create goldlocks.tf

---
+++ goldlocks.tf
@@ -0,0 +1,18 @@
+# ---------------------------------------
+# Helm Install - Goldilocks
+# ---------------------------------------
+resource "helm_release" "goldilocks" {
+  name       = "goldilocks"
+  repository = "https://charts.fairwinds.com/stable"
+  chart      = "goldilocks"
+  namespace  = kubernetes_namespace.goldilocks.metadata[0].name
+  version    = "6.3.4"  # check for latest at https://charts.fairwinds.com/stable
+
+  values = [
+    file("${path.module}/goldilocks-values.yaml")
+  ]
+
+  depends_on = [
+    kubectl_manifest.metrics_server
+  ]
+}
