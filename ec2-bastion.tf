327b3ac80394eb4ecfec494dabbd35ccdbd56e08
sundeepreddy123 <r.sundeepreddy786@gmail.com>
2025-11-14 16:38:08 +0100

    Update ec2-bastion.tf

--- ec2-bastion.tf
+++ ec2-bastion.tf
@@ -2,88 +2,93 @@
 #### EC2 Bastion Creation - linux Host
 #########################################
 
-module "ec2_bastion"{
+module "ec2_bastion_linux" {
   depends_on = [
     module.network,
     module.rds
   ]
-  source  = "./modules/terrafrom-aws-ec2-bastion"
-  version = "1.0.0"
-  # project_info  = local.project_info
 
-  vpc_info   =  {
-    cidr_blocks  =  module.network.cidr_block
-    subnets      =  module.network.private_subnets
-    vpc_id       =  module.network.vpc_id
+  source  = "./modules/terraform-aws-ec2-bastion"
+  version = "1.0.0"
+
+  vpc_info = {
+    vpc_id      = module.network.vpc_id
+    subnets     = module.network.private_subnets
+    cidr_blocks = module.network.cidr_block
   }
 
-  ec2_bastion_info  =  {
-    ami_id          =  var.instance_ami_linux
-    name            =  "linux-bastion"
-    platform        =  "linux"
-    instance_type   =  var.instance_type_linux
+  ec2_bastion_info = {
+    name          = "linux-bastion"
+    ami_id        = var.instance_ami_linux
+    platform      = "linux"
+    instance_type = var.instance_type_linux
 
-    egress_security_groups   =  {
-      egress_rds  =  {
-        description  = "to the world"
-        protocol     =  "-1"
-        type         =  "egress"
-        cidr_blocks  =  ["0.0.0.0/0"]
+    egress_security_groups = {
+      rds = {
+        description = "Allow outbound to world"
+        protocol    = "-1"
+        type        = "egress"
+        cidr_blocks = ["0.0.0.0/0"]
+      }
     }
   }
 
-  allow_access  =  {
-    rds  =  true
-    s3   =  true
+  allow_access = {
+    rds = true
+    s3  = true
   }
 
-  user_extra_data  =  [
-    "curl https"//packages
-
-    ]
-  }
+  user_extra_data = [
+    "sudo yum update -y",
+    "sudo yum install -y amazon-ssm-agent"
+  ]
 }
+
 
 #########################################
 #### EC2 Bastion Creation - windows Host
 #########################################
 module "ec2_bastion_windows" {
-  depends_on  =  [
+  depends_on = [
     module.network,
     module.rds
   ]
 
-  source     =  "./module/terrafrom-aws-ec2-bastion"
-  version    =  "1.0.0"
+  source  = "./modules/terraform-aws-ec2-bastion"
+  version = "1.0.0"
 
-  # project_info      = local.project_info
-
-
-  vpc_info  =  {
-    cidr_blocks      =  module.network.cidr_block
-    subnets          =  module.network.private_subnets
-    vpc_id           =  module.network.vpc_id
+  vpc_info = {
+    vpc_id      = module.network.vpc_id
+    subnets     = module.network.private_subnets
+    cidr_blocks = module.network.cidr_block
   }
 
-  ec2_bastion_info   =  {
-    ami_id        =  var.instance_ami_windows
-    name          =  "windows-bastion"
-    platform      =  "windows"
+  ec2_bastion_info = {
+    name     = "windows-bastion"
+    ami_id   = var.instance_ami_windows
+    platform = "windows"
 
-
-    egress_security_group   =  {
-      egress_rds  =  {
-        description  =  "to the world"
-        protocol     =  "-1"
-        type         = "egress"
-        cidr_blocks  =  ["0.0.0.0/0"]
+    egress_security_groups = {
+      rds = {
+        description = "Allow outbound"
+        protocol    = "-1"
+        type        = "egress"
+        cidr_blocks = ["0.0.0.0/0"]
       }
     }
+  }
 
-    allow_access  =  {
-      rds  =  true
-      s3   =  true
-    }
+  allow_access = {
+    rds = true
+    s3  = true
+  }
+
+  user_extra_data = [
+    "<powershell>",
+    "Install-WindowsFeature -Name RSAT-AD-PowerShell",
+    "</powershell>"
+  ]
+}
 
 
     user_extra_data  =  [
