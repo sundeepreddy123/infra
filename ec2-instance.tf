d906b5a0ff7a4e5b4683798b604e107b0aec9b1d
sundeepreddy123 <r.sundeepreddy786@gmail.com>
2025-11-14 17:41:05 +0100

    Update ec2-instance.tf

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -61,3 +61,97 @@ resource "aws_security_group" "public_ec2_sg" {
     Env  = var.env
   }
 }
+
+
+resource "aws_instance" "linux_ec2" {
+  ami                            =  var.linux_ami_id
+  instance_type                  = var.instance_type
+  subnet_id                      =  aws_subnet.public[0].id
+  associate_public_ip_address    =  true
+  vpc_security_group_ids         =  [aws_security_group.dev_linux_sg.id]
+
+  # USERDATA: Enable password login + create user
+  user_data  =  <<EQF
+#!/bin/bash
+
+# Enable password login
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config
+
+# Create root user
+useradd root
+
+# Set password
+echo "root:Password123#" | chpasswd
+
+# Restart SSH
+systemctl restart sshd
+EOF
+
+  tags = {
+    Name = "${var.env}-linux-dev"
+    Env  = var.env
+  }
+}
+
+resource "aws_security_group" "dev_linux_sg" {
+  name   = "${var.env}-dev-linux-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    description = "SSH (dev only)"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]   # OK for dev only
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "windows_ec2" {
+  ami                         = var.windows_ami_id   # Windows Server 2022/2019
+  instance_type               = var.windows_type
+  subnet_id                   = aws_subnet.public[0].id
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.dev_windows_sg.id]
+
+  # THIS ENABLES PASSWORD LOGIN FOR WINDOWS
+  user_data = <<EOF
+<powershell>
+net user Administrator "Password123!" 
+</powershell>
+EOF
+
+  tags = {
+    Name = "${var.env}-windows-dev"
+    Env  = var.env
+  }
+}
+
+resource "aws_security_group" "dev_windows_sg" {
+  name   = "${var.env}-dev-windows-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    description = "RDP (dev only)"
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]   # OK only for dev
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
