875b69563c8b278bc894ca657a89a2d338a83ff0
sundeep reddy raju <r.sundeepreddy786@gmail.com>
2026-02-04 13:19:20 +0530

    Create  waf.tf

---
+++ waf.tf
@@ -0,0 +1,54 @@
+resource "aws_wafv2_web_acl" "web_acl" {
+  name      =  "myapp-web-acl"
+  description  =  "WAF for myapp.com"
+  scope        =  "REGIONAL"
+
+  default_action  {
+    allow {}
+
+}
+
+visibility_config  {
+  cloudwatch_metrics_enabled    =  true
+  metric_name                   =  "myapp-waf"
+
+}
+
+rule  {
+  name  = "SQLInjectionRule"
+  priority  = 1
+
+  action {
+    block {}
+
+}
+
+visibility_config {
+    cloudwatch_metrics_enabled  =  true
+    metric_name                 =  "SQLInjectionRule"
+    sampled_requests_enabled    =  true
+  }
+}
+
+rule {
+  name  =  "XSSRule"
+  priorty  =  2
+
+  action  {
+    block {}
+}
+
+statement {
+  managed_rule_group_statement {
+    name      =  "AWSMangedRuleXSSRulesSet
+    vendor_name  =  "AWS"
+  }
+}
+
+visibility_config {
+  cloudwatch_metrics_enabled  = true
+  metric_name                 =  "XSSRule"
+  sampled_requests_enabled    =  true
+    }
+  }
+}
